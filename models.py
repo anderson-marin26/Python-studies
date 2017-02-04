8ccdc2eb62c50600e1224ac38b5ce5bd1fbd423c
Anderson <dam.marinramos@gmail.com>
2017-02-04 15:16:37 -0200

    Added encapsulation

--- models.py
+++ models.py
@@ -5,6 +5,22 @@ class Profile(object):
 		self.name = name
 		self.phone = phone
 		self.company = company
+		self.__likes = 0
 
 	def print(self):
 		print('Nome: %s, Telefone: %s, Empresa: %s' % (self.name, self.phone, self.company))
+
+	def like(self):
+		self.__likes+= 1
+
+	def get_likes(self):
+		return self.__likes
+
+class Data(object):
+   def __init__(self, day, month, year):
+      self.day = day
+      self.month = month
+      self.year = year
+
+   def imprime(self):
+      print('%s/%s/%s' % (self.day, self.month, self.year))
